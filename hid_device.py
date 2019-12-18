57c90127dcf1d8a084d4732b440b5007660ca0e0
smp4488 <smp4488@aol.com>
2019-12-18 06:19:41 -0500

    usb commands update

---
+++ hid_device.py
@@ -0,0 +1,20 @@
+import struct
+
+import hid
+import time
+
+vid = 0x0c45
+pid = 0x8075
+
+def main():
+    with hid.Device(vid, pid) as h:
+        print(f'Device manufacturer: {h.manufacturer}')
+        print(f'Product: {h.product}')
+        print(f'Serial Number: {h.serial}')
+
+        # data = struct.pack("<II", 0x5A8942AA, 0xCC6271FF)
+        # print(h.send_feature_report(data))
+
+
+if __name__ == "__main__":
+    main()
